39942fde2d79c29ce12a2b872e68af2a07f3d6d2
DmitryKhitrin <32079217+DmitryKhitrin@users.noreply.github.com>
2018-12-18 12:44:00 +0300

    Added js login check in modal

--- js/index.js
+++ js/index.js
@@ -4,11 +4,29 @@ var Gl = window.Gl;
 GL.appointmentModal = document.querySelector('.appointment-modal');
 GL.mapButton = document.querySelector('.map-form-button');
 GL.modalCloseBtn = document.querySelector('.appointment-close');
+GL.modalForm = document.querySelector('.appointment-form');
+GL.modalLogin = GL.modalForm.querySelector('[name=user-email]');
+GL.modalUserName = GL.modalForm.querySelector('[name=user-name]');
+GL.modalUserAbout = GL.modalForm.querySelector('[name=user-text]');
 
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 GL.mapButton.addEventListener('click', function (e) {
   e.preventDefault();
   GL.appointmentModal.classList.add('modal-show');
+  if (storage) {
+    GL.modalLogin.value = storage;
+    GL.modalUserName.focus();
+  } else {
+    GL.modalLogin.focus();
+  }
 });
 
 GL.closeModal = function (e) {
@@ -22,3 +40,14 @@ window.addEventListener('keydown', function(e) {
     GL.closeModal(e);
   }
 });
+
+GL.modalForm.addEventListener('submit', function (e) {
+  if (!GL.modalLogin.value) {
+    e.preventDefault();
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", GL.modalLogin.value);
+    }
+  }
+
+})
