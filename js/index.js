f99a59f05aba6c2b31ef19f93e6c7119dfa6fd23
DmitryKhitrin <32079217+DmitryKhitrin@users.noreply.github.com>
2018-12-18 10:52:39 +0300

    Make searchbutton more a11y/ And added css hover to it.

--- js/index.js
+++ js/index.js
@@ -1,23 +1,9 @@
 window['GL'] = {};
 var Gl = window.Gl;
 
-GL.searchSvg = document.querySelector('.search-button svg path').classList;
-GL.searchButton = document.querySelector('.user-search-popup');
 GL.appointmentModal = document.querySelector('.appointment-modal');
 GL.mapButton = document.querySelector('.map-form-button');
 GL.modalCloseBtn = document.querySelector('.appointment-close');
-
-
-
-GL.searchButton.addEventListener('mouseover', function(e) {
-  e.preventDefault();
-  GL.searchSvg.add('hover-search-fill');
-});
-
-GL.searchButton.addEventListener('mouseout', function(e) {
-  e.preventDefault();
-  GL.searchSvg.remove('hover-search-fill');
-});
 
 
 GL.mapButton.addEventListener('click', function (e) {
